8f43a880449fdf5ae0daf3d81a2482d3137d6b45
GreenNeptune <silentwisdom75@gmail.com>
2020-10-29 21:55:32 +0200

    Modify axios baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-ccb69/us-central1/api'
+  baseURL: 'http://localhost:5001/clone-9b54b/us-central1/api'
 })
 
 export default instance;
